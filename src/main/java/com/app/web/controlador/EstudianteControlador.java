d275b9868bc430665a24d0007887bccc6153ef7f
victor <victor17rf@gmail.com>
2025-10-15 11:17:25 -0600

    Actualización del archivo controlador

--- src/main/java/com/app/web/controlador/EstudianteControlador.java
+++ src/main/java/com/app/web/controlador/EstudianteControlador.java
@@ -5,8 +5,6 @@ import com.app.web.servicio.EstudianteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,14 +19,7 @@ public class EstudianteControlador {
     public ResponseEntity<List<Estudiante>> listarEstudiantes(){
         return new ResponseEntity<>(servicio.listarTodosLosEstudiantes(),HttpStatus.OK);//nos retorna el archivo estudiantes
     }
-    /*
-    @GetMapping("/estudiantes/nuevo")
-    public  String mostrarFormularioDeRegistrarEstuduante(Model modelo){
-        Estudiante estudiante = new Estudiante();
-        modelo.addAttribute("estudiante", estudiante);
-        return "crear_estudiante";
-    }
-    */
+
     @PostMapping("/estudiantes")
     public ResponseEntity<Estudiante> guardarEstudiante(@RequestBody Estudiante estudiante){ /*que son los parametros del metodo*/
         Estudiante nuevoEstudiante = servicio.guardarEstudiante(estudiante);
@@ -36,12 +27,6 @@ public class EstudianteControlador {
         return  new ResponseEntity<>(nuevoEstudiante, HttpStatus.CREATED);  /*que esta pasando en el return*/
 
     }
-    /*
-    @GetMapping("/estudiantes/editar/{id}") qué quiere decir ésto
-    public String mostrarFormularioDespEditar(@PathVariable Long id, Model modelo) {
-        modelo.addAttribute("estudiante", servicio.obtenerEstudiantePorId(id));
-        return "editar_estudiante"; por qué devuelve esto
-    }*/
 
     @PostMapping("/estudiante/{id}")
     public  ResponseEntity<Estudiante> actualizarEstudiante(@PathVariable Long id, @RequestBody Estudiante estudiante){
